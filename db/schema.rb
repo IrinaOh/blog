7e53a29edc81be76c3af484174898dab4343da2a
Irina O'Hara <irina.v.ohara@gmail.com>
2017-01-08 00:23:34 -0700

    users can create posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205190156) do
+ActiveRecord::Schema.define(version: 20170108071213) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "title"
+    t.string  "message"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fname"
